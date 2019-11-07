eef140d87a34fd72598f9b80ff5bc0d6ff560290
zombityu <vagasi.ityu@gmail.com>
2019-11-07 13:12:34 +0100

    feat (SetupRoles)

--- buyforus/buyforus/ApplicationContext.cs
+++ buyforus/buyforus/ApplicationContext.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using buyforus.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +9,20 @@ using System.Threading.Tasks;
 
 namespace buyforus
 {
-    public class ApplicationContext:DbContext
+    public class ApplicationContext:IdentityDbContext<User>
     {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<IdentityRole>().HasData(
+                new IdentityRole { Name = "Admin", NormalizedName = "Admin".ToUpper() },
+                new IdentityRole { Name = "Donator", NormalizedName = "Donator".ToUpper() },
+                new IdentityRole { Name = "Organization", NormalizedName = "Organization".ToUpper() }
+            );
+        }
     }
 }
