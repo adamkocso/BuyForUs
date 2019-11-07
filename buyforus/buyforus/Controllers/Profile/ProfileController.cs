17cbe9ebc4311b3b2e5e7c44d3f434ea7f31de82
Adam Kocso <adam@kocso.com>
2019-11-07 13:05:00 +0100

    feat (Profile controller)

---
+++ buyforus/buyforus/Controllers/Profile/ProfileController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace buyforus.Controllers.Profile
+{
+    public class ProfileController : Controller
+    {
+        [HttpGet("/userprofile")]
+        public IActionResult UserProfile()
+        {
+            return View();
+        }
+
+        [HttpGet("/orgprofile")]
+        public IActionResult OrgProfile()
+        {
+            return View();
+        }
+    }
+}
