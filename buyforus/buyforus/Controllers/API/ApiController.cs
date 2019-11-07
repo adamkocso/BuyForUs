85a8c3e8c43c7bbaba1e7bfe6a6cb0ae9eddc647
B707H <tothbalint90@gmail.com>
2019-11-07 19:53:20 +0100

    feat(Api): controller created

---
+++ buyforus/buyforus/Controllers/API/ApiController.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace buyforus.Controllers.API
+{
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        [HttpPut("api/reservation")]
+        public async Task<ActionResult> ReserveHotel([FromBody] ApiReservationViewModel model)
+        {
+    }
+}
---
+++ buyforus/buyforus/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace buyforus.Models
+{
+    public class Product
+    {
+        public long BedId { get; set; }
+        public string ProductName { get; set; }
+        public int Amount { get; set; }
+        public int Price { get; set; }
+    }
+}
