5ec463aca91987949abb085cb60ee151e0dbd43a
zombityu <vagasi.ityu@gmail.com>
2019-11-07 14:37:52 +0100

    feat (ImageService)

---
+++ buyforus/buyforus/Services/IBlobService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Azure.Storage.Blob;
+using System.Threading.Tasks;
+
+
+namespace buyforus.Services
+{
+    public interface IBlobService
+    {
+        Task<CloudBlobContainer> GetBlobContainer();
+    }
+}
---
+++ buyforus/buyforus/Services/IImageService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace buyforus.Services
+{
+    public interface IImageService
+    {
+        Task UploadAsync(IFormFileCollection files, long Id);
+        List<string> Validate(IFormFileCollection files, ViewModel newHotel);
+    }
+}
